3aa374e67a318c3d18980ed6df8fee0ae855486f
Ugochukwu <vuonyenze@gmail.com>
2020-01-18 06:01:18 -0500

    added more functions

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -4,6 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Application {
 
+        public int countWords(String words) {
+        	String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 	public Application() {
 		System.out.println ("Inside Application");
 	}
@@ -11,6 +15,8 @@ public class Application {
 	// method main() ALWAYS the APPLICATION entry point
 	public static void main (String[] args) {
 		System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+    		int count = app.countWords("I have four words");
+		System.out.println("Word Count: " + count);
 	}
 }
